eb8cbff802d81d1555284dd7333df600d3f6d09b
Evelin Rodriguez Suarez <evelin992009@gmail.com>
2020-10-20 20:53:16 +0000

    mas decisiones

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 edad = input("ingese edad :")
+if int(edad) < 17:
+  print ("cuba libre")
+elif int(edad) <= 25:
+  print("tequila")
+elif int(edad) <= 35:
+  print("coca cola")
+elif int(edad) <= 48:
+  print("cualquiera")
+else:
+  print ("leche lelel")
+
+
+
+
+
+
+
+
+
+
+'''
+edad = input("ingese edad :")
 if int(edad) < 18:
   print ("por nada del mundo")
 else:
  print ("no problem")
+ '''
